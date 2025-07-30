5fc3fdf310f3389195e80d54ed8ca54ec50b32b6
GeorgelucianRay <george.laluciu@icloud.com>
2025-07-30 09:57:35 +0200

    Actualizar Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -60,7 +60,7 @@ const LogoutIcon = () => (
   </svg>
 );
 const MenuIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="28" height "28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+  <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
     <line x1="4" x2="20" y1="12" y2="12"></line>
     <line x1="4" x2="20" y1="6" y2="6"></line>
     <line x1="4" x2="20" y1="18" y2="18"></line>
@@ -136,7 +136,6 @@ const Layout = ({ children, backgroundClassName }) => {
 
   return (
     <div className={wrapperClass}>
-      {/* fundalul și overlay-ul sunt randate doar dacă se primește backgroundClassName */}
       {backgroundClassName && (
         <div className={styles.backgroundContainer}>
           <div className={`${styles.backgroundImage} ${styles[backgroundClassName]}`} />
