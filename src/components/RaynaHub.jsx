20a6b800cb43ebd981dbd6d62b149b61f1251063
GeorgelucianRay <george.laluciu@icloud.com>
2025-08-15 23:45:03 +0200

    Actualizar RaynaHub.jsx

--- src/components/RaynaHub.jsx
+++ src/components/RaynaHub.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../AuthContext';
-import styles from './RaynaHub.module.css';
+import styles from './Chatbot.module.css';
 
 // Icon X minimal
 const CloseIcon = () => (
