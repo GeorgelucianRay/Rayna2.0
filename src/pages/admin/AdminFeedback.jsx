dc2b5b57e15aba123bd1a83e0a84025e489dd703
GeorgelucianRay <george.laluciu@icloud.com>
2025-09-28 00:02:37 +0200

    Actualizar AdminFeedback.jsx

--- src/pages/admin/AdminFeedback.jsx
+++ src/pages/admin/AdminFeedback.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { supabase } from '../../supabaseClient';
-import './AdminFeedback.css';
+import './AdminFeedback.module.css';
 
 export default function AdminFeedback() {
   const [feedbacks, setFeedbacks] = useState([]);
