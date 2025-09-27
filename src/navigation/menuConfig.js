8a8d90ae50b8f48b5cedc598f61e90825ccac818
GeorgelucianRay <george.laluciu@icloud.com>
2025-09-27 23:26:16 +0200

    Actualizar menuConfig.js

--- src/navigation/menuConfig.js
+++ src/navigation/menuConfig.js
@@ -31,6 +31,7 @@ const dispecerMenu = [
   { id: '/calculadora-nomina',  text: 'Calculadora Nómina',  icon: 'calc'  },
   { id: '/gps',                 text: 'GPS',                 icon: 'gps'   },
   { id: '/taller',              text: 'Taller',              icon: 'wrench'},
+  { id: '/admin/feedback',    text: 'Feedback', icon: 'users' },
 ];
 const adminExtra = [
   { id: '/gps-pro',             text: 'GPS Pro',             icon: 'gps'   },
