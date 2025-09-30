53b8df32e0967d195f4573617967b8931cff38ce
GeorgelucianRay <george.laluciu@icloud.com>
2025-10-01 01:35:01 +0200

    Actualizar index.js

--- src/components/chat/actions/index.js
+++ src/components/chat/actions/index.js
@@ -1,8 +1,9 @@
 // src/components/chat/actions/index.js
-export { default as handleStatic } from "./handleStatic.jsx";
 export { default as handleDialog } from "./handleDialog.jsx";
-export { default as handleOpenCamera } from "./handleOpenCamera.jsx";
-export { default as handleShowAnnouncement } from "./handleShowAnnouncement.jsx";
-export { default as handleGpsNavigate } from "./handleGpsNavigate.jsx";
 export { default as handleGpsInfo } from "./handleGpsInfo.jsx";
 export { default as handleGpsLists } from "./handleGpsLists.jsx";
+export { default as handleGpsNavigate } from "./handleGpsNavigate.jsx";
+export { default as handleOpenCamera } from "./handleOpenCamera.jsx";
+export { default as handleShowAnnouncement } from "./handleShowAnnouncement.jsx";
+export { default as handleStatic } from "./handleStatic.jsx";
+export * from "./handleProfileStuff.jsx"; // ⬅️ nou
