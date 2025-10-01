c0b00d6380148e4dbc9dba6f5771728d6e4143d8
GeorgelucianRay <george.laluciu@icloud.com>
2025-10-01 20:52:26 +0200

    Crear index.js

---
+++ src/nlu/index.js
@@ -0,0 +1,5 @@
+export { normalize } from "./normalize.js";
+export { detectIntent } from "./detect.js";
+export { detectLanguage } from "./lang.js";
+// dacă vrei să reutilizezi ajutoarele în teste/debug:
+export { includesAny, hasToken, fuzzyEq } from "./fuzzy.js";
