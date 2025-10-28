28a32bab1aed459305f7ea3f4d7f927556ef7aab
GeorgelucianRay <george.laluciu@icloud.com>
2025-10-28 15:19:48 +0100

    Actualizar propRegistry.js

--- src/components/depot/map/world/propRegistry.js
+++ src/components/depot/map/world/propRegistry.js
@@ -1,27 +1,20 @@
-import { makeRoundabout }  from './prefabs/Roundabout.js'; // +++ NOU
+import { makeRoundabout } from './prefabs/Roundabout.js';
 
 export const PROP_TYPES = [
-  { key: 'road.segment',   label: 'Șosea 12×40 m' },
-  { key: 'road.roundabout',label: 'Sens giratoriu Ø40 / bandă 12' }, // +++ NOU
-  { key: 'fence.panel',    label: 'Panou gard 2 m' },
-  { key: 'hill.tile',      label: 'Bucată munte' },
-  { key: 'tree',           label: 'Copac' },
-  { key: 'building.box',   label: 'Clădire (box)' },
+  { key: 'road.segment', label: 'Șosea 12×40 m' },
+  { key: 'road.roundabout', label: 'Sens giratoriu Ø40 / bandă 12' },
+  { key: 'fence.panel', label: 'Panou gard 2 m' },
+  { key: 'hill.tile', label: 'Bucată munte' },
+  { key: 'tree', label: 'Copac' },
+  { key: 'building.box', label: 'Clădire (box)' },
 ];
 
 export function createMeshFor(type, opts = {}) {
   switch (type) {
     case 'road.segment':
       return makeRoadSegment({ w: 12, d: 40, h: 0.02, ...opts });
-
-    case 'road.roundabout': // +++ NOU
-      return makeRoundabout({
-        outerR: 20,   // diametru 40 m
-        ringW: 12,    // lățime carosabil = lățimea drumului tău
-        h: 0.02,
-        texturePath: '/textures/lume/Drumuri.jpg',
-        ...opts
-      });
+    case 'road.roundabout':
+      return makeRoundabout({ outerR: 20, ringW: 12, h: 0.02, ...opts });
 
     case 'fence.panel':
       return makeFencePanel({ L: 2, H: 1.6, ...opts });
