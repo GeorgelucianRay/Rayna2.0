328e96cc4503c1c3f60ea828379fb15c3c45c37a
GeorgelucianRay <george.laluciu@icloud.com>
2025-11-04 12:39:19 +0100

    Actualizar main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './AuthContext';
 import App from './App.jsx';
-import { AuthProvider } from './AuthContext.jsx'; // Importăm provider-ul
-import './index.css';
+import CrashCatcher from './CrashCatcher.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider> {/* Îmbrăcăm App în AuthProvider */}
+  <BrowserRouter>
+    <AuthProvider>
+      <CrashCatcher>
         <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
+      </CrashCatcher>
+    </AuthProvider>
+  </BrowserRouter>
 );
